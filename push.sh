c96fbf66b61873bf98a27927b9ae106a701f76f4
frey <1007921963@qq.com>
2024-10-12 07:58:46 +0800

    2024-10-12

--- push.sh
+++ push.sh
@@ -1,6 +1,4 @@
  now=$(date "+%Y-%m-%d")
- echo "Change Directory to D:\MyNote"
- cd D:\\MyNote
  echo "Starting add-commit-pull-push..."
  git add -A && git commit -m "$now" && git pull && git push
  echo "Finish!"
